cddbd47cab92ed05e7077ee2106110b98f027301
kimgeonsu <kundoo0412@gmail.com>
2024-11-14 02:37:46 +0000

    [#3] Feat : areparse로 config 파일 받아서 학습시키는 train 파일 추가

--- Ultralytics/train.py
+++ Ultralytics/train.py
@@ -1,5 +1,12 @@
+import argparse
 from ultralytics import YOLO
 
-model = YOLO("yolov11x-seg.pt")
+# config 파일 경로를 입력받기 위한 인자 파서 설정
+parser = argparse.ArgumentParser()
+parser.add_argument('--cfg', type=str, required=True, help='config 파일의 경로를 입력하세요')
+args = parser.parse_args()
 
-model.train(data="data.yaml", epochs=100)
+model = YOLO("yolo11x-seg.pt")
+
+model.train(cfg=args.cfg)
+
