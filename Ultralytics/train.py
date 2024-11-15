952fc0a7598ef5f245bb98e29231a259acc8b6c3
kimgeonsu <kundoo0412@gmail.com>
2024-11-15 06:47:38 +0000

    [#3] Feat : inference 추가

--- Ultralytics/train.py
+++ Ultralytics/train.py
@@ -1,12 +1,17 @@
 import argparse
 from ultralytics import YOLO
+import wandb
 
 # config 파일 경로를 입력받기 위한 인자 파서 설정
 parser = argparse.ArgumentParser()
 parser.add_argument('--cfg', type=str, required=True, help='config 파일의 경로를 입력하세요')
 args = parser.parse_args()
 
+# wandb 초기화
+wandb.init(project="handbone-semantic-segmentation")
+
 model = YOLO("yolo11x-seg.pt")
 
-model.train(cfg=args.cfg)
+# wandb 로깅 활성화
+model.train(cfg=args.cfg, project="handbone-semantic-segmentation")
 
