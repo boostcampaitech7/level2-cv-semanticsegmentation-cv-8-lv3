2a9c9e1aca9a4959c95c75e5ccc39657ed51edbd
kimgeonsu <kundoo0412@gmail.com>
2024-11-18 13:18:53 +0900

    dataset validation 수정

--- base/dataset.py
+++ base/dataset.py
@@ -18,6 +18,7 @@ CLASSES = [
     'Triquetrum', 'Pisiform', 'Radius', 'Ulna',
 ]
 
+
 class XRayDataset(Dataset):
     def __init__(self, fnames, labels, image_root, label_root, fold=0, transforms=None, is_train=True):
         self.transforms = A.Compose(transforms)
@@ -28,80 +29,82 @@ class XRayDataset(Dataset):
         self.class2ind = {v: i for i, v in enumerate(CLASSES)}
         self.ind2class = {v: k for k, v in self.class2ind.items()}
         self.num_classes = len(CLASSES)
-        
+
         groups = [osp.dirname(fname) for fname in fnames]
-        
+
         # dummy label
         ys = [0] * len(fnames)
-        
+
         gkf = GroupKFold(n_splits=5)
-        
+
         filenames = []
         labelnames = []
         for i, (x, y) in enumerate(gkf.split(fnames, ys, groups)):
             if self.is_train:
                 if i == self.validation_fold:
-                    continue        
+                    continue
                 filenames += list(fnames[y])
                 labelnames += list(labels[y])
-            
+
             else:
+                if i != self.validation_fold:
+                    continue
                 filenames = list(fnames[y])
-                labelnames = list(labels[y])
-                break
-        
+        labelnames = list(labels[y])
+
         self.fnames = filenames
         self.labels = labelnames
-    
+
     def __len__(self):
         return len(self.fnames)
-    
+
     def __getitem__(self, item):
         image_name = self.fnames[item]
         image_path = osp.join(self.image_root, image_name)
-        
+
         image = cv2.imread(image_path)
         image = image / 255.
-        
+
         label_name = self.labels[item]
         label_path = osp.join(self.label_root, label_name)
-        
+
         # (H, W, NC) 모양의 label을 생성합니다.
         label_shape = tuple(image.shape[:2]) + (len(CLASSES), )
         label = np.zeros(label_shape, dtype=np.uint8)
-        
+
         # label 파일을 읽습니다.
         with open(label_path, "r") as f:
             annotations = json.load(f)
         annotations = annotations["annotations"]
-        
+
         # 클래스 별로 처리합니다.
         for ann in annotations:
             c = ann["label"]
             class_ind = self.class2ind[c]
             points = np.array(ann["points"])
-            
+
             # polygon 포맷을 dense한 mask 포맷으로 바꿉니다.
             class_label = np.zeros(image.shape[:2], dtype=np.uint8)
             cv2.fillPoly(class_label, [points], 1)
             label[..., class_ind] = class_label
-        
+
         if self.transforms is not None:
-            inputs = {"image": image, "mask": label} if self.is_train else {"image": image}
+            inputs = {"image": image, "mask": label} if self.is_train else {
+                "image": image}
             result = self.transforms(**inputs)
-            
+
             image = result["image"]
             label = result["mask"] if self.is_train else label
 
         # to tenser will be done later
         image = image.transpose(2, 0, 1)    # channel first 포맷으로 변경합니다.
         label = label.transpose(2, 0, 1)
-        
+
         image = torch.from_numpy(image).float()
         label = torch.from_numpy(label).float()
-            
+
         return image, label
-    
+
 
 class XRayInferenceDataset(Dataset):
     def __init__(self, fnames, image_root, transforms=None):
@@ -109,25 +112,25 @@ class XRayInferenceDataset(Dataset):
         self.image_root = image_root
         self.transforms = transforms
         self.ind2class = {i: v for i, v in enumerate(CLASSES)}
-    
+
     def __len__(self):
         return len(self.fnames)
-    
+
     def __getitem__(self, item):
         image_name = self.fnames[item]
         image_path = osp.join(self.image_root, image_name)
-        
+
         image = cv2.imread(image_path)
         image = image / 255.
-        
+
         if self.transforms is not None:
             inputs = {"image": image}
             result = self.transforms(**inputs)
             image = result["image"]
 
         # to tenser will be done later
-        image = image.transpose(2, 0, 1)  
-        
+        image = image.transpose(2, 0, 1)
+
         image = torch.from_numpy(image).float()
-            
+
         return image, image_name
